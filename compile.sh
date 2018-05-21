54cb665ddac27f443591b1d21a6848cbb0cec3fc
Okuno Zankoku <zankoku.okuno@gmail.com>
2018-05-21 09:00:47 -0400

    faster double-width multiply

--- compile.sh
+++ compile.sh
@@ -10,4 +10,4 @@ OFILE=${OBJDIR}/$(basename ${CFILE} | sed 's/\.c$/.o/')
 DEFAULT_CFLAGS=-O2
 CFLAGS=${DEFAULT_CFLAGS} ${CFLAGS}
 
-cc -c -std=c99 -I${INCLUDEDIR} ${CFLAGS} -o ${OFILE} ${CFILE}
+cc -c -I${INCLUDEDIR} ${CFLAGS} -o ${OFILE} ${CFILE}
